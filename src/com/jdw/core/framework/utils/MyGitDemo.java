bce4ecb2ec1497280acf3f590b47cd5e589c3f1e
Bhargavikanneganti <bhargavi.kannegantii@gmail.com>
2020-04-16 09:21:21 +0530

    added new sop statement

--- src/com/jdw/core/framework/utils/MyGitDemo.java
+++ src/com/jdw/core/framework/utils/MyGitDemo.java
@@ -5,5 +5,7 @@ public class MyGitDemo
 	public static void main(String[] args) {
 		System.out.println("Hello Git How are you.. Are you ready to Commit.....?");
 		System.out.println("Yes i am ready to Commit.....?");
+		System.out.println("hello");
+		System.out.println("hi");
 	}
 }
